4b3d80d1ae084877d453ba6425db2a4aa1ab709c
Ibuki-Y <ibu.love.drum@gmail.com>
2022-10-21 13:57:17 +0900

    Setup docker

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   //   origin: '*',
   //   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
   // });
-  await app.listen(3001);
+  await app.listen(5000);
 }
 bootstrap();
